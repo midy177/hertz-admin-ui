dc841328d3061bfe1acc97e1b743aef9e5f17186
medy <1228022817@qq.com>
2024-07-31 14:53:47 +0800

    chron: 使用hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import type { App } from 'vue';
 
 import { createRouter } from 'vue-router';
@@ -17,7 +17,8 @@ getRouteNames(basicRoutes);
 // 创建一个可以被 Vue 应用程序使用的路由实例
 export const router = createRouter({
   // 创建一个历史记录。
-  history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
+  //history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
+  history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
   // 应该添加到路由的初始路由列表。
   routes: basicRoutes as unknown as RouteRecordRaw[],
   // 是否应该禁止尾部斜杠。默认为假
